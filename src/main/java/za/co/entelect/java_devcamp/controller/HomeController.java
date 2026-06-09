e352235e9666979e394f98af24281338c7f5d4c0
Thashvir Rajcoomar <thashvirr@gmail.com>
2026-06-09 10:21:22 +0200

    add: logging on home page

--- src/main/java/za/co/entelect/java_devcamp/controller/HomeController.java
+++ src/main/java/za/co/entelect/java_devcamp/controller/HomeController.java
@@ -1,13 +1,22 @@
 package za.co.entelect.java_devcamp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 	@GetMapping("/")
 	public String home() {
+		logger.info("Home page requested");
+		logger.error("Error occurred");
+		logger.warn("Warning occurred");
+		logger.debug("Debug message");
+		logger.trace("Trace message");
 		return "index";
 	}
 
