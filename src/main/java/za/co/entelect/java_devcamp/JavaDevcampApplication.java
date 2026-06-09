9768e81f4faf476145a78b8a8f029119a0e7f9ad
Thashvir Rajcoomar <thashvirr@gmail.com>
2026-06-09 09:50:00 +0200

    init

---
+++ src/main/java/za/co/entelect/java_devcamp/JavaDevcampApplication.java
@@ -0,0 +1,13 @@
+package za.co.entelect.java_devcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevcampApplication.class, args);
+	}
+
+}
